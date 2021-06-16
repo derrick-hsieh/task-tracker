348748784cab390ac40efe2587b7d036eaf26a3b
derrick-hsieh <67884550+derrick-hsieh@users.noreply.github.com>
2021-06-16 17:32:34 +0800

    first commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,23 @@
+import {Task} from './Task';
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'May 5that 2 pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Kay Appointment',
+        day: 'July 5that 2 pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Christina Appointment',
+        day: 'June 5that 2 pm',
+        reminder: false,
+    },
+    
+]
